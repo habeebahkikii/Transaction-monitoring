f3537117cfa1c8317797a46959da9c146c39cf04
habeebahkikii <131429991+habeebahkikii@users.noreply.github.com>
2025-01-22 07:17:06 +0100

    1_💰_Transaction.py

--- pages/1_💰_Transaction.py
+++ pages/1_💰_Transaction.py
@@ -29,7 +29,8 @@ if uploaded_file is not None:
 
     columns_to_drop = ['Transaction_ID', 'Auth_code']
     dfpreclean = dfpreclean.drop(columns=[col for col in columns_to_drop if col in dfpreclean.columns], inplace=False)
-    
+    dfpreclean.drop(['Transaction_ID', 'Auth_code'], axis=1, inplace=True, errors='ignore')
+
     dfpreclean2 = dfpreclean[dfpreclean['Success'] == 1]
     dfpreclean2["Transaction_Notes"].fillna("N/A", inplace = True)
 
